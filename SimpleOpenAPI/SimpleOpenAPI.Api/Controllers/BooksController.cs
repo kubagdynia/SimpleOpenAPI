af3a5a6f19ef39aece21adebb387a658b8083304
Jakub Kurłowicz <kubagdynia@gmail.com>
2020-08-21 10:40:35 +0200

    Initial project structure

---
+++ SimpleOpenAPI/SimpleOpenAPI.Api/Controllers/BooksController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleOpenAPI.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet("")]
+        public ActionResult<string> GetBook()
+        {
+            return Ok("It's ok");
+        }
+    }
+}
